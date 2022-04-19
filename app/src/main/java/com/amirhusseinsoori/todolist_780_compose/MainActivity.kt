b6cc584bb109e91d17f68919e2670c0790030b6e
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-04-19 18:06:33 +0430

    show simpleList

--- app/src/main/java/com/amirhusseinsoori/todolist_780_compose/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/todolist_780_compose/MainActivity.kt
@@ -3,16 +3,19 @@ package com.amirhusseinsoori.todolist_780_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.amirhusseinsoori.todolist_780_compose.ui.theme.ToDoList_780_ComposeTheme
 
 class MainActivity : ComponentActivity() {
+    private val data: ArrayList<String> = arrayListOf("ali", "reza", "gholam")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +25,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(data)
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(list: List<String> = emptyList()) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(modifier = Modifier, onClick = { }) {
+            Text(text = "Add To Do")
+        }
+        LazyColumn() {
+            items(list) {
+                TodoItemList(item = it)
+            }
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ToDoList_780_ComposeTheme {
-        Greeting("Android")
+fun TodoItemList(item: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        elevation = 8.dp
+    ) {
+        Column(modifier = Modifier) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                text = "title : ".plus(item),
+                textAlign = TextAlign.Start
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(110.dp)
+                    .padding(5.dp),
+                text = "description : ",
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp), text = "date time",
+                textAlign = TextAlign.End
+            )
+        }
     }
+
 }
