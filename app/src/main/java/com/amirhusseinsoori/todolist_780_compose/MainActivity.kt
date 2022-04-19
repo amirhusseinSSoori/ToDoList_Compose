47facd1cab87c494a6fb482654475d692c864590
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-04-19 20:53:55 +0430

    initial base SwipeAble

--- app/src/main/java/com/amirhusseinsoori/todolist_780_compose/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/todolist_780_compose/MainActivity.kt
@@ -1,31 +1,51 @@
 package com.amirhusseinsoori.todolist_780_compose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.amirhusseinsoori.todolist_780_compose.ui.theme.ToDoList_780_ComposeTheme
 
+var id: Int = 0
+
 class MainActivity : ComponentActivity() {
-    private val data: ArrayList<String> = arrayListOf("ali", "reza", "gholam")
+
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val notesList = remember {
+                mutableStateListOf<NotesItem>(
+                )
+            }
             ToDoList_780_ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainScreen(data)
+
+                    MainScreen(notesList)
                 }
             }
         }
@@ -33,23 +53,72 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun MainScreen(list: List<String> = emptyList()) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(modifier = Modifier, onClick = { }) {
-            Text(text = "Add To Do")
+fun MainScreen(notesList: MutableList<NotesItem>) {
+    Column {
+        Button(
+            onClick = {
+                notesList.add(NotesItem(id++, "ali"))
+            },
+            modifier = Modifier
+        ) {
+            Text(text = "ADD")
         }
-        LazyColumn() {
-            items(list) {
-                TodoItemList(item = it)
-            }
+        LazyColumn {
+            items(notesList, { notesList: NotesItem -> notesList.id }) { item ->
+                val dismissState = rememberDismissState()
+                if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                    notesList.remove(item)
+                }
+                SwipeToDismiss(
+                    state = dismissState,
+                    modifier = Modifier
+                        .padding(vertical = Dp(1f)),
+                    directions = setOf(
+                        DismissDirection.EndToStart
+                    ),
+                    dismissThresholds = { direction ->
+                        FractionalThreshold(if (direction == DismissDirection.EndToStart) 0.1f else 0.05f)
+                    },
+                    background = {
+                        val color by animateColorAsState(
+                            when (dismissState.targetValue) {
+                                DismissValue.Default -> Color.White
+                                else -> Color.Red
+                            }
+                        )
+                        val alignment = Alignment.CenterEnd
+                        val icon = Icons.Default.Delete
+                        val scale by animateFloatAsState(
+                            if (dismissState.targetValue == DismissValue.Default) 0.75f else 1f
+                        )
 
+                        Box(
+                            Modifier
+                                .fillMaxSize()
+                                .background(color)
+                                .padding(horizontal = Dp(20f)),
+                            contentAlignment = alignment
+                        ) {
+                            Icon(
+                                icon,
+                                contentDescription = "Delete Icon",
+                                modifier = Modifier.scale(scale)
+                            )
+                        }
+                    },
+                    dismissContent = {
+                        TodoItemList(item = item.title)
+                    }
+                )
+                Divider(Modifier.fillMaxWidth(), Color.DarkGray)
+            }
         }
     }
 }
+
 
 @Composable
 fun TodoItemList(item: String) {
@@ -84,3 +153,5 @@ fun TodoItemList(item: String) {
     }
 
 }
+
+data class NotesItem(var id: Int, var title: String)
