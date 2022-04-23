6235001055cb17ad38329d287974bfcf8d0065c5
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-04-23 14:13:33 +0430

    add ui test for compose

---
+++ app/src/androidTest/java/com/amirhusseinsoori/todolist_780_compose/MainActivityTest.kt
@@ -0,0 +1,91 @@
+package com.amirhusseinsoori.todolist_780_compose
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.amirhusseinsoori.addtodo.AddToDoViewModel
+import com.amirhusseinsoori.addtodo.AddToListScreen
+import com.amirhusseinsoori.showtodo.ShowToDoScreen
+import com.amirhusseinsoori.showtodo.ShowToDoViewModel
+import com.amirhusseinsoori.todolist.ToDoViewModel
+import com.amirhusseinsoori.todolist.TodoScreen
+import com.amirhusseinsoori.todolist_780_compose.main.InitialNavGraph
+import com.amirhusseinsoori.todolist_780_compose.main.theme.ToDoList_780_ComposeTheme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private lateinit var activity: MainActivity
+
+
+    @Before
+    fun setup() {
+        composeTestRule.activityRule.scenario.onActivity {
+            activity = it
+        }
+    }
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Test
+    fun activityTest() {
+        composeTestRule.setContent {
+            ToDoList_780_ComposeTheme {
+                val navController: NavHostController = rememberAnimatedNavController()
+                val viewModel: ToDoViewModel = hiltViewModel()
+                InitialNavGraph(navController, viewModel)
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Test
+    fun toDoListTest() {
+        composeTestRule.setContent {
+            ToDoList_780_ComposeTheme {
+                val navController: NavHostController = rememberAnimatedNavController()
+                val viewModel: ToDoViewModel = hiltViewModel()
+                TodoScreen(navController = navController, viewModel = viewModel)
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Test
+    fun showToDoTest() {
+        composeTestRule.setContent {
+            ToDoList_780_ComposeTheme {
+                val viewModel: ShowToDoViewModel = hiltViewModel()
+                ShowToDoScreen(showToDoViewModel = viewModel)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Test
+    fun addToDoTest() {
+        composeTestRule.setContent {
+            ToDoList_780_ComposeTheme {
+                val viewModel: AddToDoViewModel = hiltViewModel()
+                val navController: NavHostController = rememberAnimatedNavController()
+                AddToListScreen(navController = navController, viewModel = viewModel)
+            }
+        }
+    }
+}
--- features/ShowToDo/src/main/java/com/amirhusseinsoori/showtodo/ShowToDoViewModel.kt
+++ features/ShowToDo/src/main/java/com/amirhusseinsoori/showtodo/ShowToDoViewModel.kt
@@ -6,20 +6,19 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.amirhusseinsoori.domain.entity.TodoModel
-import com.amirhusseinsoori.showtodo.util.sendArgument
 import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ShowToDoViewModel @Inject constructor(
-     savedStateHandle: SavedStateHandle,  gson: Gson
+    savedStateHandle: SavedStateHandle, gson: Gson
 ) : ViewModel() {
     var todoModelState by mutableStateOf(TodoModel())
 
     init {
-        gson.fromJson(savedStateHandle.sendArgument("details"), TodoModel::class.java).apply {
-            todoModelState = this
+        gson.fromJson(savedStateHandle.get<String>("details"), TodoModel::class.java)?.let {
+            todoModelState = it
         }
     }
 }
